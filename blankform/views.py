f0e3f1a62a9a04dfaa7acb2df6706cc23726b824
taksenov <taksenov@gmail.com>
2014-10-03 17:24:43 +0600

    Правка ошибки POST 404 на тестовой версии сервера в интернете.

--- blankform/views.py
+++ blankform/views.py
@@ -62,121 +62,113 @@ def comment(request):
     if not request.method == 'POST':
         raise Http404
 
-    try:
-        errors = []
-        error = ''
-        my_session = request.session.session_key
+    errors = []
+    error = ''
+    my_session = request.session.session_key
 
-        all_questions = connection.cursor()
-        all_questions.execute ("""
-            SELECT q.*
-            FROM   blankform_question q
-            ;
-        """)
-        result_all_questions = all_questions.fetchall()
+    all_questions = connection.cursor()
+    all_questions.execute ("""
+        SELECT q.*
+        FROM   blankform_question q
+        ;
+    """)
+    result_all_questions = all_questions.fetchall()
 
-        all_answers = connection.cursor()
-        all_answers.execute ("""
-            SELECT * FROM   blankform_answer;
-        """)
-        result_all_answers = all_answers.fetchall()
+    all_answers = connection.cursor()
+    all_answers.execute ("""
+        SELECT * FROM   blankform_answer;
+    """)
+    result_all_answers = all_answers.fetchall()
 
-        if request.method == 'POST':
-            if not request.POST.get('answer1', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer2', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer3', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer4', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer5', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer6', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer7', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer8', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer9', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer10', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer11', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('answer12', ''):
-                errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
-                error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
-            if not request.POST.get('customcomment', ''):
-                customcomment_text = ''
-            else:
-                customcomment_text = request.POST['customcomment']
-            if not errors:
+    if request.method == 'POST':
+        if not request.POST.get('answer1', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer2', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer3', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer4', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer5', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer6', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer7', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer8', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer9', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer10', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer11', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('answer12', ''):
+            errors.append('Произошла ошибка! Необходимо ответить на все вопросы анкеты.')
+            error = 'Произошла ошибка! Необходимо ответить на все вопросы анкеты.'
+        if not request.POST.get('customcomment', ''):
+            customcomment_text = ''
+        else:
+            customcomment_text = request.POST['customcomment']
+        if not errors:
 
-                clientsession__data = clientsession(
-                    clientsession = my_session,
-                    customcomment = customcomment_text
+            clientsession__data = clientsession(
+                clientsession = my_session,
+                customcomment = customcomment_text
+                )
+            clientsession__data.save()
+
+            for question_var in result_all_questions:
+                answer__text = request.POST['answer' + str(question_var[0]) ]
+                question_id_var = question.objects.get(pk=question_var[0])
+                if u'Удовлетворен' in answer__text:
+                    answer__id_var = answer.objects.get(pk=1)
+                elif u'Частично удовлетворен' in answer__text:
+                    answer__id_var = answer.objects.get(pk=2)
+                elif u'Не удовлетворен' in answer__text:
+                    answer__id_var = answer.objects.get(pk=3)
+                clientsanswers__data = clientsanswers(
+                    client_id = clientsession__data,
+                    question_id = question_id_var,
+                    answer_id = answer__id_var
                     )
-                clientsession__data.save()
+                clientsanswers__data.save()
 
-                for question_var in result_all_questions:
-                    answer__text = request.POST['answer' + str(question_var[0]) ]
-                    question_id_var = question.objects.get(pk=question_var[0])
-                    if u'Удовлетворен' in answer__text:
-                        answer__id_var = answer.objects.get(pk=1)
-                    elif u'Частично удовлетворен' in answer__text:
-                        answer__id_var = answer.objects.get(pk=2)
-                    elif u'Не удовлетворен' in answer__text:
-                        answer__id_var = answer.objects.get(pk=3)
-                    clientsanswers__data = clientsanswers(
-                        client_id = clientsession__data,
-                        question_id = question_id_var,
-                        answer_id = answer__id_var
-                        )
-                    clientsanswers__data.save()
+            templ = get_template('commentform.html')
+            html = templ.render(RequestContext(
+                                         request,
+                                         {
+                                         'user': request.user,
+                                         }))
+            return HttpResponse(html)
 
-                try:
-                    templ = get_template('commentform.html')
-                    html = templ.render(RequestContext(
-                                                 request,
-                                                 {
-                                                 'user': request.user,
-                                                 }))
-                    return HttpResponse(html)
-                except ValueError:
-                    raise Http404()
 
-            try:
-                templ = get_template('blankform.html')
-                html = templ.render(
-                                    RequestContext(request,
-                                                   {
-                                                   'result_all_answers':  result_all_answers,
-                                                   'result_all_questions': result_all_questions,
-                                                   'my_session': my_session,
-                                                   'user': request.user,
-                                                   'errors': errors,
-                                                   'error': error,
-                                                   }
-                                                   )
-                                   )
-                return HttpResponse(html)
-            except ValueError:
-                raise Http404()
+        templ = get_template('blankform.html')
+        html = templ.render(
+                            RequestContext(request,
+                                           {
+                                           'result_all_answers':  result_all_answers,
+                                           'result_all_questions': result_all_questions,
+                                           'my_session': my_session,
+                                           'user': request.user,
+                                           'errors': errors,
+                                           'error': error,
+                                           }
+                                           )
+                           )
+        return HttpResponse(html)
 
-    except:
-        raise Http404()
 
 
 
